f44ba6ba213ec5ef88a2bcdb6de5b7900b00bf2b
ABoss <ashleyjeanbostrom@gmail.com>
2023-01-19 15:56:43 -0700

    prodroutes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -63,6 +63,13 @@ router.get('/:id', (req, res) => {
 
 // create new product
 router.post('/', (req, res) => {
+  Product.create({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    category_id: req.body.category_id,
+    tagIds: req.body.tagIds,
+  })
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -137,6 +144,24 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbProductData) => {
+      if (!dbProductData) {
+        res.status(404).json({ message: 'No product found with this id' });
+        return;
+      }
+      res.json(dbProductData);
+    }
+    )
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    }
+    );
 });
 
 module.exports = router;
