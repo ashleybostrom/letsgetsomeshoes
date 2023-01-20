5846e2bf66ae6a0175ca22a21e062bf44dc64dd6
ABoss <ashleyjeanbostrom@gmail.com>
2023-01-20 15:05:09 -0700

    set empty password to connectionj

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,12 @@
-const Sequelize = require('sequelize');
 require('dotenv').config();
+const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
+      user: 'root',
+      password: '',
       dialect: 'mysql',
       port: 3306,
       dialectOptions: {
