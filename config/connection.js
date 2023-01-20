35ee49ed3dbb0afb443032b462e4468e36423d03
ABoss <ashleyjeanbostrom@gmail.com>
2023-01-20 15:17:42 -0700

    trying to get access to sequelize removed port from connectionjs

--- config/connection.js
+++ config/connection.js
@@ -5,10 +5,7 @@ const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
-      user: 'root',
-      password: '',
       dialect: 'mysql',
-      port: 3306,
       dialectOptions: {
         decimalNumbers: true,
       },
