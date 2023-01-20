7dae37bb3a4adb9f0a7ad5130290955384a3beae
ABoss <ashleyjeanbostrom@gmail.com>
2023-01-20 16:52:31 -0700

    finish error message and syntax in categoryroutes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -60,6 +60,10 @@ router.put('/:id', async (req, res) => {
     }
     res.status(200).json(categoryData);
   }
+  catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
