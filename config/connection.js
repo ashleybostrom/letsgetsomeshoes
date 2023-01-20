df8a2518e0ab2cf930054397853054d6c7bcb145
ABoss <ashleyjeanbostrom@gmail.com>
2023-01-20 15:22:36 -0700

    cant connect sequelize

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,6 @@ const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
-      user: 'root',
-      password: 'password',
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
