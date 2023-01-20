7e412246bf4eb822b3b2f092f1396869a256e487
ABoss <ashleyjeanbostrom@gmail.com>
2023-01-20 15:22:04 -0700

    debug sequelize connection

--- config/connection.js
+++ config/connection.js
@@ -5,6 +5,8 @@ const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
+      user: 'root',
+      password: 'password',
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
